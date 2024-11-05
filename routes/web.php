52b32eef12b2f886d2e8e4b1bd72ab1bfb0d8361
AlumG <AlumG@majada.es>
2024-11-05 16:06:27 +0100

    A10

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
     dd(opcache_get_status());
     return view('welcome');
 });
